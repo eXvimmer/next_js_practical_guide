684d20186ab62749501924742753ba8ecaa995bc
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-12 15:59:04 +0330

    01: navigate programmatically.

--- 01_file_based_routing_course/pages/clients/[id]/index.tsx
+++ 01_file_based_routing_course/pages/clients/[id]/index.tsx
@@ -5,9 +5,21 @@ function ClientsProjectPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // router.push(`/clients/client1/projectA`);
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "client1",
+        clientprojectid: "projectA",
+      },
+    });
+  }
+
   return (
     <div>
       <h1>Clients Projects Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
