cb02c7ac1bec37f06178fa15ecb326aae6d73f7d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-12 15:51:33 +0330

    01: navigate to dynamic routes.

--- 01_file_based_routing_course/pages/clients/index.tsx
+++ 01_file_based_routing_course/pages/clients/index.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
+const clients = [
+  { id: 1, name: "Mustafa" },
+  { id: 2, name: "Yoonjin" },
+  { id: 3, name: "Malena" },
+  { id: 4, name: "Emily" },
+  { id: 5, name: "Maya" },
+];
+
 function ClientsPage() {
   return (
     <div>
       <h1>Clients Page</h1>
+      <ul>
+        {clients.map((c) => (
+          <li key={c.id}>
+            <Link href={`/clients/${c.id}`}>{c.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
