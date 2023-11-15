b71df3b7bc26639b1f071fc333eb2402b65d688f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-15 15:58:22 +0330

    05: add getStaticProps function to the home page.

--- 05_data_fetching_course/pages/index.tsx
+++ 05_data_fetching_course/pages/index.tsx
@@ -1,9 +1,25 @@
-export default function Home() {
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((p) => (
+        <li key={p.id}>{p.title}</li>
+      ))}
     </ul>
   );
 }
+
+export const getStaticProps = (async (/* context */) => {
+  return {
+    props: {
+      products: [
+        { id: 1, title: "Product 1" },
+        { id: 2, title: "Product 2" },
+        { id: 3, title: "Product 3" },
+      ],
+    },
+  };
+}) satisfies GetStaticProps;
