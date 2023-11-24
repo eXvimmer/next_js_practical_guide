24877bf1c0f572a887f803db69c08e37e3988728
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-24 17:44:02 +0330

    03: replace client-side with server-side rendering.

--- 03_file_based_routing_project_course/pages/events/[...slug].tsx
+++ 03_file_based_routing_project_course/pages/events/[...slug].tsx
@@ -2,69 +2,17 @@ import EventList from "@/components/events/EventList";
 import ResultsTitle from "@/components/events/ResultsTitle";
 import Button from "@/components/ui/Button";
 import ErrorAlert from "@/components/ui/ErrorAlert";
-import { useRouter } from "next/router";
-import { Event } from "@/types";
-import { useEffect, useState } from "react";
 import supabase from "@/services/supabase";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-export default function FilteredEventsPage() {
-  const [events, setEvents] = useState<Event[]>([]);
-  const [isValid, setIsValid] = useState(true);
-  const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
-  const { slug } = router.query;
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setIsValid(true);
-        if (!slug || !slug.length || typeof slug === "string") {
-          setIsValid(false);
-          return;
-        }
-        const [year, month] = slug;
-        const numYear = +year;
-        const numMonth = +month;
-        if (
-          isNaN(numYear) ||
-          isNaN(numMonth) ||
-          numMonth < 1 ||
-          numMonth > 12 ||
-          slug.length !== 2
-        ) {
-          setIsValid(false);
-          return;
-        }
-        const startDate = new Date(numYear, numMonth - 1, 1);
-        const endDate = new Date(numYear, numMonth, 0);
-        const startDateString = startDate.toISOString().split("T")[0];
-        const endDateString = endDate.toISOString().split("T")[0];
-        const { data, error } = await supabase
-          .from("events")
-          .select()
-          .gte("date", startDateString)
-          .lte("date", endDateString);
-        if (error) {
-          console.error(error);
-          setIsValid(false);
-          return;
-        }
-        setEvents(data);
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, [slug]);
-
-  if (isLoading) {
-    return <p className="center">Loading...</p>;
-  }
-
-  if (!isValid || !slug || typeof slug === "string") {
+export default function FilteredEventsPage(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>,
+) {
+  if (props.error || !props.date) {
     return (
       <>
         <ErrorAlert>
-          <p>Invalid filter. Please adjust the filter values.</p>
+          <p style={{ textTransform: "capitalize" }}>{props.error}</p>
         </ErrorAlert>
         <div className="center">
           <Button link="/events">Show All Events</Button>
@@ -73,7 +21,7 @@ export default function FilteredEventsPage() {
     );
   }
 
-  if (!events.length) {
+  if (!props.events.length) {
     return (
       <>
         <ErrorAlert>
@@ -86,15 +34,65 @@ export default function FilteredEventsPage() {
     );
   }
 
-  const [year, month] = slug;
-  const numYear = +year;
-  const numMonth = +month;
-  const date = new Date(numYear, numMonth - 1);
-
   return (
     <>
-      <ResultsTitle date={date} />
-      <EventList items={events} />
+      <ResultsTitle
+        date={new Date(props?.date?.year, props?.date?.month - 1)}
+      />
+      <EventList items={props.events} />
     </>
   );
 }
+
+export const getServerSideProps = (async (context) => {
+  const slug = context.params?.slug;
+  if (!slug || !slug.length || typeof slug === "string") {
+    return {
+      notFound: true,
+    };
+  }
+  const [year, month] = slug;
+  const numYear = +year;
+  const numMonth = +month;
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    slug.length !== 2
+  ) {
+    return {
+      props: {
+        error: "invalid filter. please adjust the filter values.",
+        events: [],
+      },
+      // redirect: {
+      //   destination: "/error"
+      // }
+    };
+  }
+  const startDate = new Date(numYear, numMonth - 1, 1);
+  const endDate = new Date(numYear, numMonth, 0);
+  const startDateString = startDate.toISOString().split("T")[0];
+  const endDateString = endDate.toISOString().split("T")[0];
+  const { data, error } = await supabase
+    .from("events")
+    .select()
+    .gte("date", startDateString)
+    .lte("date", endDateString);
+  if (error || !data) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      error: "",
+      events: data,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  };
+}) satisfies GetServerSideProps;
