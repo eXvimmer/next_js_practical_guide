74cba8425df46ca9ecac8ac50c798322ac5a1b85
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-27 16:26:09 +0330

    07: create form and api route and handle form submission and save the data to a file.

--- 07_api_routes/src/pages/api/feedback.ts
+++ 07_api_routes/src/pages/api/feedback.ts
@@ -1,5 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ msg: "feedback works" });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { email, text }: { email?: string; text?: string } = req.body;
+    if (!email || !text) {
+      return res
+        .status(400)
+        .json({ message: "email and feedback text are required" });
+    }
+    const newFeedback = {
+      id: crypto.randomUUID(),
+      email,
+      text,
+    };
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const buffer = fs.readFileSync(filePath);
+    const data = JSON.parse(buffer);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    return res.status(201).json({ message: "success", feedback: newFeedback });
+  }
+  res.setHeader("Allow", "POST");
+  return res.status(405).json({ message: "method not allowed" });
 }
