bc315774af7a940cfc0545a9e196192aa03f5799
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-12-01 00:27:36 +0330

    03: show notifications when creating a comment.

--- 03_file_based_routing_project_course/components/input/comments.tsx
+++ 03_file_based_routing_project_course/components/input/comments.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import styles from "./comments.module.css";
 import { Comment } from "@/types";
+import NotificationContext from "@/store/notification-context";
 
 function Comments({ eventId }: { eventId: string }) {
   const [showComments, setShowComments] = useState(false);
+  const { showNotification } = useContext(NotificationContext);
 
   function toggleCommentsHandler() {
     setShowComments((s) => !s);
   }
 
   function addCommentHandler(comment: Partial<Comment>) {
+    showNotification({
+      title: "Saving your comment",
+      message: "Please wait",
+      status: "pending",
+    });
+    setShowComments(false);
     fetch(`/api/events/${eventId}/comments`, {
       method: "POST",
       body: JSON.stringify(comment),
@@ -21,9 +29,22 @@ function Comments({ eventId }: { eventId: string }) {
       },
     })
       .then((res) => res.json())
-      .then(() => {
-        setShowComments(false);
-      });
+      .then((data) => {
+        if (data.success) {
+          showNotification({
+            title: "Success!",
+            message: data.message || "Comment created",
+            status: "success",
+          });
+        } else {
+          showNotification({
+            title: "Error!",
+            message: data.message || "Something went wrong",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => setShowComments(true));
   }
 
   return (
