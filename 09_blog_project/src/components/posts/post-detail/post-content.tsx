438a5c2a0b98affa98df9898af9df571b86c1cd3
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-12-02 19:30:28 +0330

    09: render code snippets from markdown.

--- 09_blog_project/src/components/posts/post-detail/post-content.tsx
+++ 09_blog_project/src/components/posts/post-detail/post-content.tsx
@@ -3,6 +3,8 @@ import Image from "next/legacy/image";
 import styles from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 import type { PostData } from "@/types";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialOceanic } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent({ post }: { post: PostData }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -25,6 +27,16 @@ function PostContent({ post }: { post: PostData }) {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+    // @ts-ignore
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter style={materialOceanic} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
--- 09_blog_project/src/posts/mastering-javascript.md
+++ 09_blog_project/src/posts/mastering-javascript.md
@@ -3,7 +3,7 @@ title: Mastering JavaScript
 excerpt: JavaScript is the most important programming language for web development. You probably don't know it well enough!
 image: mastering-js-thumb.png
 isFeatured: false
-date: '2021-10-30'
+date: "2021-10-30"
 ---
 
 JavaScript powers the web - it's **the** most important programming language you need to know as a web developer.
@@ -20,4 +20,14 @@ function printBasics() {
 printBasics();
 ```
 
-Learn more about it [here](https://academind.com).
+```cpp
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, world.\n";
+  return 0;
+}
+
+```
+
+Learn more about it [here](https://github.com/exvimmer).
