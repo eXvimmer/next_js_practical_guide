57a8bf93e74ec45bae3c90138fb484aee9b1dafa
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-12-07 16:24:13 +0330

    11: send signup requests from the front-end.

--- 11_auth_project/src/components/auth/auth-form.tsx
+++ 11_auth_project/src/components/auth/auth-form.tsx
@@ -1,33 +1,71 @@
-import { useState } from "react";
+import { FormEventHandler, useRef, useState } from "react";
 import styles from "./auth-form.module.css";
 
+async function createUser(email: string, password: string) {
+  const response = await fetch(`/api/auth/signup`, {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "something went wrong");
+  }
+  return data;
+}
+
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isInLoginMode, setIsInLoginMode] = useState(true); // login/create mode
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
 
   function switchAuthModeHandler() {
-    setIsLogin((s) => !s);
+    setIsInLoginMode((s) => !s);
   }
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    if (!email || !password) {
+      return;
+    }
+    if (isInLoginMode) {
+      // TODO: log the user in
+    } else {
+      try {
+        const result = await createUser(email, password);
+        console.log(result); // TODO: add a notification
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <section className={styles.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form>
+      <h1>{isInLoginMode ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={handleSubmit}>
         <div className={styles.control}>
           <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required />
+          <input type="email" id="email" required ref={emailRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="password">Your Password</label>
-          <input type="password" id="password" required />
+          <input type="password" id="password" required ref={passwordRef} />
         </div>
         <div className={styles.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          <button>{isInLoginMode ? "Login" : "Create Account"}</button>
           <button
             type="button"
             className={styles.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isInLoginMode
+              ? "Create new account"
+              : "Login with existing account"}
           </button>
         </div>
       </form>
--- 11_auth_project/src/pages/api/auth/signup.ts
+++ 11_auth_project/src/pages/api/auth/signup.ts
@@ -3,36 +3,46 @@ import { hashPassword } from "@/lib/auth";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { email, password }: { email?: string; password?: string } = req.body;
-    if (!email || !email.includes("@")) {
-      res.status(422).json({
+  switch (req.method) {
+    case "POST":
+      try {
+        const { email, password }: { email?: string; password?: string } =
+          req.body;
+        if (!email || !email.includes("@")) {
+          res.status(422).json({
+            success: false,
+            message: "invalid email",
+          });
+          return;
+        }
+        if (!password || password.length < 8) {
+          res.status(422).json({
+            success: false,
+            message:
+              "invalid password, password should be at least 8 characters long",
+          });
+          return;
+        }
+        const hashedPassword = await hashPassword(password);
+        const { error } = await supabase
+          .from("users")
+          .insert({ email, password: hashedPassword });
+        res.status(error ? 400 : 201).json({
+          success: !error,
+          message: error ? error.message : "user created",
+        });
+      } catch (err) {
+        res.status(500).json({
+          success: false,
+          message: err instanceof Error ? err.message : "something went wrong",
+        });
+      }
+      break;
+    default:
+      res.status(405).setHeader("Allow", "POST").json({
         success: false,
-        message: "invalid email",
+        message: "method not allowed",
       });
-      return;
-    }
-    if (!password || password.length < 8) {
-      res.status(422).json({
-        success: false,
-        message:
-          "invalid password, password should be at least 8 characters long",
-      });
-      return;
-    }
-    const hashedPassword = await hashPassword(password);
-    const { error } = await supabase
-      .from("users")
-      .insert({ email, password: hashedPassword });
-    res.status(error ? 400 : 201).json({
-      success: !error,
-      message: error ? error.message : "user created",
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err instanceof Error ? err.message : "something went wrong",
-    });
   }
 }
 
