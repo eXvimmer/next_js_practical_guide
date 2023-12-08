da882f792de32b9e0c26caebb51c8595742e8600
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-12-09 00:19:44 +0330

    11: add server side page guard for the profile page.

--- 11_auth_project/src/components/profile/user-profile.tsx
+++ 11_auth_project/src/components/profile/user-profile.tsx
@@ -1,19 +1,7 @@
-import { useSession } from "next-auth/react";
 import ProfileForm from "./profile-form";
 import styles from "./user-profile.module.css";
-import { useRouter } from "next/router";
 
 function UserProfile() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === "loading") {
-    return <p className={styles.profile}>Loading...</p>;
-  } else if (!session) {
-    router.push("/auth");
-    return;
-  }
-
   return (
     <section className={styles.profile}>
       <h1>Your User Profile</h1>
--- 11_auth_project/src/pages/profile.tsx
+++ 11_auth_project/src/pages/profile.tsx
@@ -1,7 +1,22 @@
-import UserProfile from '../components/profile/user-profile';
+import { GetServerSideProps } from "next";
+import UserProfile from "../components/profile/user-profile";
+import { getSession } from "next-auth/react";
 
 function ProfilePage() {
   return <UserProfile />;
 }
+
+export const getServerSideProps = (async (context) => {
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} }; // we could pass session, but we don't need it.
+}) satisfies GetServerSideProps;
 
 export default ProfilePage;
